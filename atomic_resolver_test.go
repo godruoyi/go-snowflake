fdb753bbece9b74ee1ebd9d72bd888b846a0f382
Godruoyi <godruoyi@163.com>
2021-04-15 11:50:51 +0800

    ✈ ignore result for AtomicResolver test

--- atomic_resolver_test.go
+++ atomic_resolver_test.go
@@ -17,13 +17,13 @@ func TestAtomicResolver(t *testing.T) {
 func BenchmarkCombinationParallel(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			snowflake.AtomicResolver(1)
+			_, _ = snowflake.AtomicResolver(1)
 		}
 	})
 }
 
 func BenchmarkAtomicResolver(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		snowflake.AtomicResolver(1)
+		_, _ = snowflake.AtomicResolver(1)
 	}
 }
